f55ecee57e83bff542a1cf8150789840a15f995e
tangxinxu <tangxinxu88@gmail.com>
2019-05-23 18:45:24 +0800

    增加猜随机数程序

--- Untitled-1.py
+++ Untitled-1.py
@@ -18,8 +18,28 @@ for char in s:
     print(s.index(char),char)
 for i in range(len(s)):
     print(s[i])
-    
-from IPython.core.interactiveshell import InteractiveShell
-InteractiveShell.ast_node_interactivity = "all"
 
-"Now is better than never.".upper
+
+for i in range(12):
+    print(i)
+
+a_list = [1,2,3]
+a_list.append(5)
+print(a_list)
+
+import random
+y = 1
+a = random.randint(0,100)
+b = int(input('请输入0-100中一个数字\n然后查看是否与电脑一样：'))
+while a != b:
+    if a > b:
+        print('你第%d输入的数字小于随机数字')
+        b = int(input('请再次输入'))
+    else:
+        print('你输入的大于电脑随机数')
+        b = int(input("请再次输入"))
+
+else:
+    print("恭喜你猜对了")
+
+
