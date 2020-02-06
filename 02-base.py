00646c367b7e0e362db1c9b597e9eb3a476bf3b9
tangxinxu <41268511+tangxinxu@users.noreply.github.com>
2020-02-06 19:01:55 +0800

    go on

---
+++ 02-base.py
@@ -0,0 +1,12 @@
+# 苹果价格
+prince_apple = float(input("请输入苹果价格:"))
+# 苹果重量
+weight_apple = float(input("请输入苹果重量："))
+# 价格计算
+money = weight_apple * prince_apple
+
+print("苹果单价%.2f元/斤，购买了%.2f斤，需要支付%.2f元" %(prince_apple,weight_apple,money))
+
+
+sacle =0.35
+print("数据比例是%.2f%%"%(sacle))
---
+++ base_01.py
@@ -0,0 +1,27 @@
+# 这是来测试用的
+print('hhhh')
+
+# 代码后增加注释 两个空格
+print('ddddd')  #hhhh
+
+'''
+哈哈哈哈哈哈哈哈哈哈哈哈哈哈测试多行注释
+'''
+apple = 8
+number = 3
+money = apple*number
+money = money - 10  #变量只在第一次出现时，是定义变量
+
+print(money)
+
+name = '小明'
+years = 18
+gender = 'male'
+height = 175
+weight = 60
+
+password = input('请输入此时此刻')
+type(int(password))
+
+
+
